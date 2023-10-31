6ba7c319e0f39ab0f076d7249581984a5d4c2e98
clinckzone <divyanshu.tiwari33@gmail.com>
2023-10-31 19:45:14 +0530

    Updates

---
+++ precache-manifest.5b7d1100345a05fc9b278f35cc2810b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf0d9104452b47190a9155fd94fd516",
+    "url": "/meme-maker/index.html"
+  },
+  {
+    "revision": "b8234ba2f5de45e3a477",
+    "url": "/meme-maker/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "ef165cb59fba3f41b02f",
+    "url": "/meme-maker/static/css/main.27517e93.chunk.css"
+  },
+  {
+    "revision": "b8234ba2f5de45e3a477",
+    "url": "/meme-maker/static/js/2.09b88ce7.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/meme-maker/static/js/2.09b88ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef165cb59fba3f41b02f",
+    "url": "/meme-maker/static/js/main.9113cb15.chunk.js"
+  },
+  {
+    "revision": "57ca37a410b1898b8b2e",
+    "url": "/meme-maker/static/js/runtime-main.274295c2.js"
+  }
+]);
